d0a7111d0942d60ca20ed7947220407e6cea1a0c
AnkitBisain <league@wts10.attlocal.net>
2018-09-30 17:28:29 -0700

    About to start magic box

--- src/Jukebox.java
+++ src/Jukebox.java
@@ -4,6 +4,8 @@
  */
 
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -11,7 +13,10 @@ import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 import javazoom.jl.player.advanced.AdvancedPlayer;
@@ -19,18 +24,43 @@ import javazoom.jl.player.advanced.AdvancedPlayer;
 /* 1. Download the JavaZoom jar from here: http://bit.ly/javazoom
  * 2. Right click your project and add it as an External JAR (Under Java Build Path > Libraries).*/
 
-public class Jukebox implements Runnable {
+public class Jukebox implements Runnable, ActionListener {
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Jukebox());
 	}
+	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton songButton = new JButton();
+	JButton songButton2 = new JButton();
+	
+	void createGUI()
+	{
+		frame.setVisible(true);
+		frame.add(panel);
+		songButton.setText("Music Box");
+		songButton2.setText("Orchestra");
+		panel.add(songButton);
+		panel.add(songButton2);
+		frame.pack();
+		
+		songButton.addActionListener(this);
+		songButton2.addActionListener(this);
+	}
+	
+	Song song = new Song("274379__modification1089__creepy-music-box.wav");
+	Song song2 = new Song("348275__bigmanjoe__fantasy-orchestra.wav");
 
-           public void run() {
+    public void run() {
 
+    	createGUI();
+    	
 		// 3. Find an mp3 on your computer or on the Internet.
 		// 4. Create a Song
 
 		// 5. Play the Song
+        	  // song.play();
 
 		/*
 		 * 6. Create a user interface for your Jukebox so that the user can to
@@ -40,13 +70,22 @@ public class Jukebox implements Runnable {
 		 * that was selected.
 		 */
           }
-	/* Use this method to add album covers to your Panel. */
+	/* Use this method to add album covers to your Panel.
 	private JLabel loadImage(String fileName) {
 		URL imageURL = getClass().getResource(fileName);
 		Icon icon = new ImageIcon(imageURL);
 		return new JLabel(icon);
 	}
+	 */
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton pressed = (JButton) e.getSource();
+		if(pressed == songButton)
+		{
+			song2.play();
+		}
+	}
 }
 
 class Song {
